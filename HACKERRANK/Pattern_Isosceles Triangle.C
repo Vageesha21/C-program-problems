6cb5f2fc8a3ebbcec742f05b6a05496d4e8a2549
vageesha <vermatejaswa51@gmail.com>
2025-03-12 22:29:51 +0530

    Pattern_Isosceles Triangle

---
+++ HACKERRANK/Pattern_Isosceles Triangle.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(n>=2){
+         for(int i=1;i<=n;++i){
+       
+           for(int k=2;k<=n-i+1;++k){
+                printf(" ");
+           }
+          
+           for(int j=1;j<=2*i-1;++j){
+                printf("*");
+           }
+         
+         printf("\n");
+    }
+    }
+        else{
+            printf("Shape Not Possible");
+        }
+
+   
+    return 0;
+}
