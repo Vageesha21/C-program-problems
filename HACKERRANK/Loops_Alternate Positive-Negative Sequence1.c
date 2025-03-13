e714ec922eba1394bfe3d0dc58a3c82e819367d6
vageesha <vermatejaswa51@gmail.com>
2025-03-13 23:47:47 +0530

    Loops_Alternate Positive-Negative Sequence1

---
+++ HACKERRANK/Loops_Alternate Positive-Negative Sequence1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+           
+            printf("%d ", i);
+        } else {
+           
+            printf("%d ", -i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
